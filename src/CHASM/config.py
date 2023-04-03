d0e26592dc0f01969f3503f0359f80f60e2221a0
ikepc1 <ikepc1@gmail.com>
2023-04-03 13:24:43 -0600

    Added config file for atmosphere parameters

---
+++ src/CHASM/config.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+from .atmosphere import Atmosphere, USStandardAtmosphere, CorsikaAtmosphere
+
+@dataclass
+class AxisConfig:
+    '''This is the container for axis config parameters'''
+    N_POINTS: int = 1000
+    ATM: Atmosphere = CorsikaAtmosphere()
